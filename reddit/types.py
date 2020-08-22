e4b7603308f0aaf3b82cfca8e81020c0810634bd
sudosnok <subsonic96@gmail.com>
2020-08-22 19:23:57 +0100

    attempting to fix ResponseData's init

--- reddit/types.py
+++ reddit/types.py
@@ -39,9 +39,9 @@ class ResponseData:
         self.posts = deque()
 
         try: # if an event loop is running, make a task and schedule it
-            loop = asyncio.get_running_loop()
-            task = loop.create_task(coro)
-            print(task, loop)
+            asyncio.get_running_loop()
+            task = asyncio.Task(coro)
+            print(task)
             self._data = data = task.result()
         except RuntimeError: # get_running_loop will raise RuntimeError, so we'll use asyncio.run
             self_data = data = asyncio.create_task(coro).result()
