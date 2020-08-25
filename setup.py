e07fe130da5663aafd15d19fd0e410648666f975
sudosnok <subsonic96@gmail.com>
2020-08-25 23:58:37 +0100

    Major modifications, version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='RAPILite',
-    version='0.1.4',
+    version='0.1.5',
     description='An asynchronous wrapper for Reddit\'s readonly API',
     long_description=long_description,
     long_description_content_type='text/markdown',
